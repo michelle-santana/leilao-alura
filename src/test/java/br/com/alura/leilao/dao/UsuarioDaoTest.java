56f40796629126d870822b74993ca7c0c5ba1b00
michelle-santana <michelle.raiadrogasil@gmail.com>
2021-06-29 23:08:54 -0300

    Testando a classe leilao e os metodos de deletar e salvar um leilao

--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -40,6 +40,16 @@ class UsuarioDaoTest {
 		criarUsuario();
 		Assert.assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("beltrano"));
 	}
+	
+	@Test
+	void deveriaRemoverUsuario() {
+		
+		Usuario usuario = criarUsuario();
+		dao.deletar(usuario);
+		
+		Assert.assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername(usuario.getNome()));
+		
+	}
 
 	private Usuario criarUsuario() {
 		Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
